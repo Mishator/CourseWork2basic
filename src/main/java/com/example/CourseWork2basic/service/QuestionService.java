8f167d231713548947ecc38974bc53eec70d40db
Mishator <voyteh89@mail.ru>
2023-08-20 19:18:09 +0300

    Создали интерфейс QuestionService

---
+++ src/main/java/com/example/CourseWork2basic/service/QuestionService.java
@@ -0,0 +1,18 @@
+package com.example.CourseWork2basic.service;
+
+import com.example.CourseWork2basic.entity.Question;
+
+import java.util.Collection;
+
+public interface QuestionService {
+
+    Question add(String question, String answer);
+
+    Question add(Question question);
+
+    Question remove(Question question);
+
+    Collection<Question> getAll();
+
+    Question getRandomQuestion();
+}
