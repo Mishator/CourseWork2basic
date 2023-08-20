4d174cc870a72eaab7b6486362f3fa375d9dc982
Mishator <voyteh89@mail.ru>
2023-08-20 18:30:15 +0300

    init commit

---
+++ src/main/java/com/example/CourseWork2basic/CourseWork2basicApplication.java
@@ -0,0 +1,13 @@
+package com.example.CourseWork2basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWork2basicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseWork2basicApplication.class, args);
+	}
+
+}
